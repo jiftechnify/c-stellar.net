53150928c8c87725b3f5770b0c18637132af4fc0
jiftechnify <jiftech.stlfy@gmail.com>
2023-06-06 22:24:06 +0900

    add animation while hiding dialog

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -8,6 +8,20 @@
   $: if (dialog && showModal) {
     dialog.showModal();
   }
+
+  let hiding = false;
+  const closeDialog = () => {
+    if (dialog === undefined) {
+      return;
+    }
+    hiding = true;
+    dialog.addEventListener("animationend", didHide, false);
+  };
+  const didHide = () => {
+    hiding = false;
+    dialog.close();
+    dialog.removeEventListener("animationend", didHide, false);
+  };
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -16,13 +30,14 @@
   on:close={() => {
     showModal = false;
   }}
-  on:click|self={() => dialog.close()}
+  on:click|self={() => closeDialog()}
+  class:hiding
 >
   <button
     class="btn-close"
     type="button"
     on:click={() => {
-      dialog.close();
+      closeDialog();
     }}
   >
     <Icon
@@ -46,22 +61,32 @@
     border-radius: 8px;
   }
   dialog[open] {
-    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+    animation: zoomin 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
   }
+  dialog.hiding {
+    animation: zoomout 0.15s;
+  }
+
   dialog::backdrop {
     background-color: rgba(0, 0, 0, 0.3);
   }
   dialog[open]::backdrop {
-    animation: fade 0.2s ease-out;
+    animation: fadein 0.2s ease-out;
+  }
+  dialog.hiding::backdrop {
+    animation: fadeout 0.15s ease-out;
   }
 
   button.btn-close {
     position: absolute;
     top: 16px;
     right: 16px;
+    display: flex;
+    align-items: center;
+    padding: 4px;
   }
 
-  @keyframes zoom {
+  @keyframes zoomin {
     from {
       transform: scale(0.9);
     }
@@ -69,12 +94,24 @@
       transform: scale(1);
     }
   }
-  @keyframes fade {
+  @keyframes zoomout {
+    to {
+      opacity: 0;
+      transform: scale(0.95);
+    }
+  }
+
+  @keyframes fadein {
     from {
       opacity: 0;
     }
     to {
       opacity: 1;
+    }
+  }
+  @keyframes fadeout {
+    to {
+      opacity: 0;
     }
   }
 </style>
