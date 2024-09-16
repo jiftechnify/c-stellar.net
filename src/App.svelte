31426ae6e644cee13cb90f2fc34942a78c437b94
jiftechnify <jiftech.stlfy@gmail.com>
2024-09-16 11:31:53 +0900

    remove nositifier

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
-  import { Route, Router } from "svelte-routing";
   import Home from "./pages/Home.svelte";
-  import Nositifier from "./pages/Nositifier.svelte";
 </script>
 
-<Router>
-  <Route path="">
-    <Home />
-  </Route>
-  <Route path="/nos">
-    <Nositifier />
-  </Route>
-</Router>
+<Home />
