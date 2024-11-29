3c2dda65f6dcb84742f4bf65e975bcebeb72d01c
jiftechnify <jiftech.stlfy@gmail.com>
2024-11-30 02:36:04 +0900

    try sveltekit SSG

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Home from "../lib/Home.svelte";
+  import "../global.scss";
+</script>
+
+<svelte:head>
+  <meta name="description" content="jiftechnify's website" />
+  <meta property="og:title" content="c-stellar.net" />
+  <meta property="og:description" content="jiftechnify's website" />
+  <meta property="og:url" content="https://c-stellar.net" />
+  <meta property="og:image" content="https://pubimgs.c-stellar.net/ogp.png" />
+  <meta property="og:type" content="website" />
+  <meta property="og:site_name" content="c-stellar.net" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@cstl_fy" />
+  <title>c-stellar.net</title>
+</svelte:head>
+<Home />
